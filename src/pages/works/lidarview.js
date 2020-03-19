f9e9bc2de5ab49226a51eb51efceb056eec2abf5
Brendon Dulam <brendon.dulam@veoneer.com>
2020-03-20 02:10:20 +0900

    client side rendering for AVS

--- src/pages/works/lidarview.js
+++ src/pages/works/lidarview.js
@@ -1,18 +1,24 @@
 import { Typography } from "@material-ui/core"
 import React from "react"
 
-import AVSAuto from "../../components/works/avs/AVSAuto"
+// import AVSAuto from "../../components/works/avs/AVSAuto"
 
 export default () => {
+  const isSSR = typeof window === "undefined"
+
   return (
-    <React.Suspense
-      fallback={
-        <Typography variant="h1" color="primary">
-          Loading page
-        </Typography>
-      }
-    >
-      <AVSAuto />
-    </React.Suspense>
+    <>
+      {!isSSR && (
+        <React.Suspense
+          fallback={
+            <Typography variant="h1" color="primary">
+              Loading page
+            </Typography>
+          }
+        >
+          Test
+        </React.Suspense>
+      )}
+    </>
   )
 }
