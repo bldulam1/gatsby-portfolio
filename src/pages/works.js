d9c6b6a5db1a87368c0380a11f549a1bccaecfcb
Brendon Dulam <brendon.dulam@veoneer.com>
2020-03-22 01:54:10 +0900

    trading in works

--- src/pages/works.js
+++ src/pages/works.js
@@ -35,10 +35,18 @@ export default () => {
           It stores the states of components in the browser's localStorage.`}
         />
         <WorksCard
+          image={require("../images/trading.jpg")}
+          link="/works/tradingsimulator"
+          title="Trading Simulator"
+          description={`This is an ongoing project, it is a trading simulation for testing trading strategies.
+          The objective of this application is to simulate a trade without using actual money.
+          The assumption for this game is that the action that a user makes does not affect the market significantly as a whole.`}
+        />
+        <WorksCard
           image={require("../images/maze.jpg")}
           link="/works/pathfinding"
           title="Path Finding Algorithm"
-          description={`This is an implementation of the A* algorithm in React.
+          description={`This is an ongoing project, it is an implementation of the A* algorithm in React.
           I was inspired to create this project after watching the Micromouse Challenge.
           The path that it finds is not yet optimized, also I am still improving the Maze builder feature.`}
         />
