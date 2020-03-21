cd0bf9fbfbb601eacf28d95c1cab5a0553d87f14
Brendon Dulam <brendon.dulam@veoneer.com>
2020-03-21 22:45:08 +0900

    stock chart

--- src/components/works/finance/TradeSimulator.js
+++ src/components/works/finance/TradeSimulator.js
@@ -1,5 +1,24 @@
 import React from "react"
+import Chart from "./ChartWithVolume"
+import { getData } from "./utils"
 
-export default () => {
-  return <div>Hello</div>
+import { TypeChooser } from "react-stockcharts/lib/helper"
+
+export default class ChartComponent extends React.Component {
+  componentDidMount() {
+    getData().then(data => {
+      this.setState({ data })
+    })
+  }
+  render() {
+    if (this.state == null) {
+      return <div>Loading...</div>
+    }
+    console.log(this.state.data)
+    return (
+      <TypeChooser>
+        {type => <Chart type={type} data={this.state.data} />}
+      </TypeChooser>
+    )
+  }
 }
