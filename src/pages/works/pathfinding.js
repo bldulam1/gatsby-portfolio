f7d57609f0684cb00769603516efee9f5d0f0248
Brendon Dulam <brendon.dulam@veoneer.com>
2020-04-17 02:40:11 +0900

    format

--- src/pages/works/pathfinding.js
+++ src/pages/works/pathfinding.js
@@ -190,7 +190,11 @@ export default () => {
 
   return (
     <Layout>
-      <SEO title="Path Finding" description="Visualization of the A star path finding algorithm" image={require("../../images/maze.jpg")} />
+      <SEO
+        title="Path Finding"
+        description="Visualization of the A star path finding algorithm"
+        image={require("../../images/maze.jpg")}
+      />
       <Box
         width="100%"
         height="85vh"
